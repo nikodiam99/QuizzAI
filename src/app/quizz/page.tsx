e1239d4b45021886bc8602d63fa1b307fe12dbc7
nikodiam99 <nikodiam99@gmail.com>
2024-06-02 22:18:05 -0400

    added quizz route

---
+++ src/app/quizz/page.tsx
@@ -0,0 +1,13 @@
+import {Button} from "@/components/ui/button";
+export default function Home() {
+    return (
+        <div className="flex flex-col flex-1">
+            <main className="flex flex-1 justify-center">
+                <h1 className="text-6xl font-bold">Hello World👋</h1>
+            </main>
+            <footer className="footer pb-9 px-6 relative mb-6">
+                <Button>Start</Button>
+            </footer>
+        </div>
+    )
+}
